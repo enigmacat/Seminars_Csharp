144c57ce5da52b33ea90d79c76e2cd3e087d5a50
enigmacat <mumoxodom27@gmail.com>
2022-08-31 21:52:02 +1000

    add solution for task 0,1,3,5,7

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//================================================================
+// # 1 Напишите программу, которая на входе принимает два числа и
+// проверяет, является ли первое число квадратом второго.
+//================================================================
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    // bool outResult = (inputNumberB * inputNumberB == inputNumberA);
+    // bool outResult = (inputNumberA / inputNumberB == inputNumberB);
+    bool outResult = (Math.Sqrt(inputNumberA) == inputNumberB);
+
+    Console.WriteLine(outResult);
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿//==========================================================
+// # 5 Напишите программу, которая на входе принимает число N,
+// а на выходе показывает все целые числа от -N до N.
+//===========================================================
+
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int inputNumberN = int.Parse(inputLineN);
+    if (inputNumberN > 0)
+    {
+        int startNumber = (-1) * inputNumberN;
+        while (startNumber < inputNumberN)
+        {
+            Console.Write(startNumber + ", ");
+            startNumber = startNumber + 1;
+        }
+        Console.Write(inputNumberN);
+    }
+}
