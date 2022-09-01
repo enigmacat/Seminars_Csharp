b07544af3a956f6787c0c405e2bb54f1f2ec6d08
enigmacat <mumoxodom27@gmail.com>
2022-09-01 12:59:16 +1000

    fix

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -3,9 +3,10 @@ if (inputLineN != null)                        // Проверка на null
 {
     int inputNumberN = int.Parse(inputLineN);  // Делаем целым
     int numberB = 2;                           // Присваиваем переменной B первое четное число
-    while (numberB <= inputNumberN)            // цикл до тех пор, пока B <= введенному числу
+    while (numberB <= inputNumberN - 2)            // цикл до тех пор, пока B <= введенному числу
     {
         Console.Write(numberB + ", ");         // Вывод на экран результатов цикла в ряд
         numberB = numberB + 2;                 // увеличиваем В на 2, пока есть соотв. условию цикла
-    }   
-}                                       
+    }
+    Console.Write(numberB);
+}
