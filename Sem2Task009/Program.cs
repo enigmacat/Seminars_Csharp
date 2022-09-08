dac0093935e873bc3063e408990c6b77f1bb70df
enigmacat <mumoxodom27@gmail.com>
2022-09-09 01:24:00 +1000

    fix

--- Sem2Task009/Program.cs
+++ Sem2Task009/Program.cs
@@ -3,21 +3,14 @@
 // из отрезка [10, 99] и показывает наибольшую цифру числа.
 //===================================================================
 
-
-
 void Variant1()
 {
     Console.WriteLine("method 1");
-
     System.Random numberGenerator = new System.Random();
-
     int number = numberGenerator.Next(10, 100);
-
     Console.WriteLine(number);
-
     int firstDigit = number / 10;
     int secondDigit = number % 10;
-
     // Variant 1
     if (firstDigit > secondDigit)
     {
@@ -31,35 +24,24 @@ void Variant1()
 
 void Variant2()                              // Тернарный оператор
 {
-Console.WriteLine("method 2");
-
+    Console.WriteLine("method 2");
     System.Random numberGenerator = new System.Random();
-
     int number = numberGenerator.Next(10, 100);
-
     Console.WriteLine(number);
-
     int firstDigit = number / 10;
     int secondDigit = number % 10;
-
-      
-      // Тернарный оператор  
+    // Тернарный оператор  
     Console.WriteLine((firstDigit > secondDigit) ? firstDigit : secondDigit);
 }
 
 void Variant3()                              // char
 {
     Console.WriteLine("method 3");
-
     System.Random numberGenerator = new System.Random();
-
     int number = numberGenerator.Next(10, 100);
-
     Console.WriteLine(number);
-
     int firstDigit = number / 10;
     int secondDigit = number % 10;
-
     // Variant 3  char
     char[] charArray = number.ToString().ToCharArray();
     Console.WriteLine(((int)charArray[0] > (int)charArray[1]) ? charArray[0] : charArray[1]);
