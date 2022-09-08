e88444888e2dbf8968cbc4133c361b4f209c3c65
enigmacat <mumoxodom27@gmail.com>
2022-09-09 00:11:44 +1000

    fix

--- Sem1Task003/Program.cs
+++ Sem1Task003/Program.cs
@@ -11,22 +11,21 @@ if (inputLine != null)
 
     string outDayOfWeek = string.Empty;
 
-    // switch (inputDayOfWeek)
-    // {
-    //     case 1: outDayOfWeek = "Понедельник"; break;
-    //     case 2: outDayOfWeek = "Вторник"; break;
-    //     case 3: outDayOfWeek = "Среда"; break;
-    //     case 4: outDayOfWeek = "Четверг"; break;
-    //     case 5: outDayOfWeek = "Пятница"; break;
-    //     case 6: outDayOfWeek = "Суббота"; break;
-    //     case 7: outDayOfWeek = "Воскресенье"; break;
-    //     default: outDayOfWeek = "Такого дня нет"; break;
-    // }
+    switch (inputDayOfWeek)
+    {
+        case 1: outDayOfWeek = "Понедельник"; break;
+        case 2: outDayOfWeek = "Вторник"; break;
+        case 3: outDayOfWeek = "Среда"; break;
+        case 4: outDayOfWeek = "Четверг"; break;
+        case 5: outDayOfWeek = "Пятница"; break;
+        case 6: outDayOfWeek = "Суббота"; break;
+        case 7: outDayOfWeek = "Воскресенье"; break;
+        default: outDayOfWeek = "Такого дня нет"; break;
+    }
 
-    outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputDayOfWeek));
+    // outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(inputDayOfWeek));
 
     Console.WriteLine(outDayOfWeek);
-
 }
 
 // string[] dayOfWeek = new string[7];
