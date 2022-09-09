814a4ae32c395411f80aa5549b77fcc276f66a32
enigmacat <mumoxodom27@gmail.com>
2022-09-10 03:31:05 +1000

    fix

--- Sem3Task019/Program.cs
+++ Sem3Task019/Program.cs
@@ -16,7 +16,7 @@ int ReadData(string line)   // чтение данных из консоли
 
 bool TestPldrm(int pal)   // проверка на палиндром
 {
-    if (pal / 10000 == pal % 10 && pal / 1000 % 10 == pal / 10 % 10)
+    if (pal / 10000 == pal % 10 && (pal / 1000) % 10 == (pal / 10) % 10)
     {
         return true;
     }
