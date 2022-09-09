f3334dc1297c2ee90631e768b145d6b09e283c20
enigmacat <mumoxodom27@gmail.com>
2022-09-09 20:55:22 +1000

    fix

--- Sem3Task020/Program.cs
+++ Sem3Task020/Program.cs
@@ -15,18 +15,20 @@ int ReadData(string line)   // чтение данных из консоли
 
 double DotDistance(int x1, int y1, int x2, int y2)
 {     // расстояние между двумя точками в 2D пространстве
-    return Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
-      // Выводим результат
+// Выводим результат
 void PrintResult(string line)
 {
     Console.WriteLine(line);
 }
 
+// Вводим координаты точек
 int x1 = ReadData("Введите координаты x1");
 int y1 = ReadData("Введите координаты y1");
 int x2 = ReadData("Введите координаты x2");
 int y2 = ReadData("Введите координаты y2");
+
 // преобразовываем в строку с округлением
-string res = Math.Round(DotDistance(x1,y1,x2,y2),3).ToString();  
+string res = Math.Round(DotDistance(x1, y1, x2, y2), 3).ToString();
 PrintResult("Расстояние между точками " + res);
